88e48fadf6a3d41c331fe1bf445350995e2bc464
Niclas <ngl@trifork.com>
2018-12-18 16:47:26 +0100

    Added Your First App Project

---
+++ Projects/Console Apps/Your First App/YourFirstApp/YourFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YourFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
