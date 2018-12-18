01a6ec44e050fcd807bec9b18a95e6224bfede91
Niclas <ngl@trifork.com>
2018-12-18 16:55:59 +0100

    Primitive datatypes

--- Projects/Console Apps/Your First App/YourFirstApp/YourFirstApp/MyClass.cs
+++ Projects/Console Apps/Your First App/YourFirstApp/YourFirstApp/MyClass.cs
@@ -1,10 +1,20 @@
-﻿namespace YourFirstApp
+﻿using System;
+
+namespace YourFirstApp
 {
     class MyClass
     {
         static void Main()
         {
-            System.Console.WriteLine("Hello World!");
+            //http://condor.depaul.edu/sjost/nwdp/notes/cs1/CSDatatypes.htm
+
+            int number = 50;
+            float anotherNumber = 5.5f;
+            double aThirdNumber = 5.5;
+            string aString = "this is a string";
+            char aChar = 'c';
+
+            Console.WriteLine(aString);
         }
     }
 }
