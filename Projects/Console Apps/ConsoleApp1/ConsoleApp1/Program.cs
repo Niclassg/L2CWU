24392b5273c1d18e9df93966ed91b34c6d89ea2a
Niclas <ngl@trifork.com>
2019-01-02 12:09:27 +0100

    003 Get your name program

--- Projects/Console Apps/ConsoleApp1/ConsoleApp1/Program.cs
+++ Projects/Console Apps/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            DateTime time;
+
+            Console.WriteLine("What is your name? ");
+            name = Console.ReadLine();
+            time = DateTime.Now;
+            Console.WriteLine($"Hello {name}, the time is now: {time.Date} at {time.TimeOfDay}");
         }
     }
 }
